c36bf7d09c99aeb493a3633c92f558b294089989
Anil Bomma <anil.bomma@walmart.com>
2021-05-23 23:02:09 -0400

    fetching jobs from client

--- graphql-job-board/client/src/JobBoard.js
+++ graphql-job-board/client/src/JobBoard.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 import { JobList } from './JobList';
-const { jobs } = require('./fake-data');
+// const { jobs } = require('./fake-data');
 
 export class JobBoard extends Component {
+  state = {
+    job: []
+  };
+
+  async componentDidMount() {
+    const job = await this.getJobs();
+    this.setState({ job });
+  }
+
+  async getJobs() {
+    let jobs = await fetch("http://localhost:9000/graphql", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "operationName": null,
+        "variables": {},
+        "query": "{ jobs { id title description company {id name description } } }"
+      })
+    });
+
+    jobs = await jobs.json();
+    return jobs.data.jobs;
+  }
+
   render() {
+    const { job } = this.state;
     return (
       <div>
         <h1 className="title">Job Board</h1>
-        <JobList jobs={jobs} />
+        <JobList jobs={job} />
       </div>
     );
   }
