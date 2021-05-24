3a96f53d8085c14fcf78771623724dbce5d601ec
Anil Bomma <anil.bomma@walmart.com>
2021-05-24 08:27:19 -0400

    fetch company details

--- graphql-job-board/client/src/CompanyDetail.js
+++ graphql-job-board/client/src/CompanyDetail.js
@@ -1,15 +1,49 @@
 import React, { Component } from 'react';
-import { companies } from './fake-data';
+// import { companies } from './fake-data';
 
 export class CompanyDetail extends Component {
   constructor(props) {
     super(props);
-    const {companyId} = this.props.match.params;
-    this.state = {company: companies.find((company) => company.id === companyId)};
+    this.state = {company: null}
+  }
+
+  async componentDidMount() {
+    const company = await this.getCompany();
+    this.setState({company});
+  }
+
+  async getCompany() {
+    let job = await fetch("http://localhost:9000/graphql", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(
+        {
+          "query": `
+          query CompanyQuery ($id: ID!){
+            company(id: $id) {
+              name
+              description
+            }
+          }
+          `,
+          variables: {
+            id: `${this.props.match.params.companyId}`
+          },
+          operationName: "CompanyQuery"
+        })
+    });
+
+    job = await job.json();
+    return job.data.company;
   }
 
   render() {
-    const {company} = this.state;
+    const { company } = this.state;
+    if (!company) {
+      return null;
+    }
     return (
       <div>
         <h1 className="title">{company.name}</h1>
--- graphql-job-board/server/resolvers.js
+++ graphql-job-board/server/resolvers.js
@@ -3,7 +3,8 @@ const db = require("./db");
 const Query = {
     greeting: () => "Hello GraphQL",
     job: (root, args) => db.jobs.get(args.id),
-    jobs: () => db.jobs.list()
+    jobs: () => db.jobs.list(),
+    company: (root, {id}) => db.companies.get(id)
 }
 
 const Job = {
