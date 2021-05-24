0b1aed671a51c755b803ee495a92ce1a771734b5
Anil Bomma <anil.bomma@walmart.com>
2021-05-24 07:05:05 -0400

    returing job by ID

--- graphql-job-board/client/src/JobDetail.js
+++ graphql-job-board/client/src/JobDetail.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { jobs } from './fake-data';
+// import { jobs } from './fake-data';
 
 export class JobDetail extends Component {
   constructor(props) {
     super(props);
-    const {jobId} = this.props.match.params;
-    this.state = {job: jobs.find((job) => job.id === jobId)};
+    this.state = { job: { company: {} } };
+  }
+
+  async getJobs() {
+    let jobs = await fetch("http://localhost:9000/graphql", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "operationName": null,
+        "variables": {},
+        "query": "{ jobs { id title description company {id name description } } }"
+      })
+    });
+
+    jobs = await jobs.json();
+    return jobs.data.jobs;
+  }
+
+  async componentDidMount() {
+    const jobs = await this.getJobs();
+    const { jobId } = this.props.match.params;
+
+    this.setState({ job: jobs.find((job) => job.id === jobId) });
   }
 
   render() {
-    const {job} = this.state;
+    const { job } = this.state;
     return (
       <div>
         <h1 className="title">{job.title}</h1>
